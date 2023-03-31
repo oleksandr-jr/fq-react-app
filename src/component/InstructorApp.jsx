69943dc1220f4f98d7fe8fcb7ab0f37002145e0e
Oleksandr <oleksandr.jr@gmail.com>
2023-03-31 17:35:21 +0200

    upd

--- src/component/InstructorApp.jsx
+++ src/component/InstructorApp.jsx
@@ -8,7 +8,7 @@ class InstructorApp extends Component {
         return (
             <Router>
                 <>
-                    <h1>Instructor Application</h1>
+                    <h1>JavaRush Instructor Application</h1>
                     <Switch>
                         <Route path="/" exact component={ListCoursesComponent} />
                         <Route path="/courses" exact component={ListCoursesComponent} />
