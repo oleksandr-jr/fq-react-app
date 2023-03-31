96927d2c500574b29b2f753ce2bf10db59aa237c
Oleksandr <oleksandr.jr@gmail.com>
2023-03-31 15:52:25 +0200

    Updates

---
+++ precache-manifest.649233c2226cc9f78b5ff38c40b26af1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1653d4b60a53dd362ef4",
+    "url": "/fq-react-app/static/css/main.1bb17859.chunk.css"
+  },
+  {
+    "revision": "1653d4b60a53dd362ef4",
+    "url": "/fq-react-app/static/js/main.81952c9a.chunk.js"
+  },
+  {
+    "revision": "56dd11cb6c2eea8c5a20",
+    "url": "/fq-react-app/static/js/runtime~main.3cad30b8.js"
+  },
+  {
+    "revision": "5f8b5468e824157514bf",
+    "url": "/fq-react-app/static/js/2.4295869a.chunk.js"
+  },
+  {
+    "revision": "a985dda8df9ac1b0e620de7f5ddf3d44",
+    "url": "/fq-react-app/index.html"
+  }
+];
